77394d6426af1117ebded2a3cebde4ce5580a099
Chuck <charleswsmith8@gmail.com>
2019-01-03 18:14:15 -0700

    First version

--- src/com/chucksmith/Main.java
+++ src/com/chucksmith/Main.java
@@ -3,24 +3,24 @@ package com.chucksmith;
 public class Main {
 
     public static void main(String[] args) {
-	boolean gameOver = true;
-	int score = 800;
-	int levelComplete = 5;
-	int bonus =100;
+		boolean gameOver = true;
+		int score = 800;
+		int levelComplete = 5;
+		int bonus =100;
 
-	if(gameOver){
-	    int finalScore = score + (levelComplete + bonus);
-	    finalScore += 1000;
-	    System.out.println("Your final score was " + finalScore);
+		if(gameOver){
+			int finalScore = score + (levelComplete + bonus);
+	    	finalScore += 1000;
+	    	System.out.println("Your final score was " + finalScore);
     }
 
-	score = 10000;
-	levelComplete = 8;
-	bonus = 200;
+		score = 10000;
+		levelComplete = 8;
+		bonus = 200;
 
-	if(gameOver){
-	    int finalScore = score + (levelComplete + bonus);
-	    System.out.println("Your final score was " + finalScore);
-    }
+		if(gameOver){
+			int finalScore = score + (levelComplete + bonus);
+	    	System.out.println("Your final score was " + finalScore);
+    	}
     }
 }
