15005bad9b553b234f07bd7136c59aa9c0891ddd
Chuck <charleswsmith8@gmail.com>
2019-01-03 19:03:43 -0700

    Update 2

--- src/com/chucksmith/Main.java
+++ src/com/chucksmith/Main.java
@@ -8,24 +8,26 @@ public class Main {
 		int levelComplete = 5;
 		int bonus = 100;
 
-		calculateScore(true, 800, levelComplete, bonus);
-
+		int returnValue = calculateScore(true, 800, levelComplete, bonus);
+		System.out.println(returnValue);
 		score = 10000;
 		levelComplete = 8;
 		bonus = 200;
 
-		calculateScore(true, score, levelComplete, bonus);
-
+		returnValue = calculateScore(true, score, levelComplete, bonus);
+		System.out.println(returnValue);
 	}
 
-	public static void calculateScore(boolean gameOver, int score, int levelComplete, int bonus) {
+	public static int calculateScore(boolean gameOver, int score, int levelComplete, int bonus) {
 
 
 		if (gameOver) {
 			int finalScore = score + (levelComplete * bonus);
 			finalScore += 1000;
 			System.out.println("Your final score was " + finalScore);
+			return finalScore;
 		}
+		return -1;
 	}
 
 }
